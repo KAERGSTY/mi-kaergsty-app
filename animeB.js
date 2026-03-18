749329032ac14cc21800f66c57567e127e242f81
KAERGSTY <137520623+KAERGSTY@users.noreply.github.com>
2026-03-17 21:49:07 -0500

    2

---
+++ animeB.js
@@ -0,0 +1,396 @@
+
+
+window.base["Bad Girl"] = {
+    nombresAlternativos: {
+        "Japonés": "バッドガール",
+        "Ingles": "Bad Girl"
+    },
+    sinopsis: "Una chica aparentemente modelo busca romper las reglas para llamar la atención, entrando en un mundo de pequeñas rebeldías y situaciones cómicas.",
+    portada: "https://a.storyblok.com/f/178900/934x1320/c23d1f5748/bad-girl-key-visual.jpg/m/filters:quality(95)format(webp)", // Pega aquí tu enlace de Google
+    generos: ["Comedia", "Escolar"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK",
+            "Latino": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Blue Lock"] = {
+    nombresAlternativos: {
+        "Japonés": "ブルーロック",
+        "Ingles": "Blue Lock"
+    },
+    sinopsis: "Tras el fracaso de Japón en el Mundial, surge un proyecto para crear al delantero más egoísta y capaz del mundo en una instalación de entrenamiento extrema.",
+    portada: "https://images.justwatch.com/poster/321707353/s718/temporada-1.jpg", 
+    generos: ["Deportes", "Acción", "Shounen"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK",
+            "Latino": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Baki 2018"] = {
+    nombresAlternativos: {
+        "Japonés": "バキ",
+        "Ingles": "Baki"
+    },
+    sinopsis: "Baki Hanma entrena intensamente para superar a su padre, mientras cinco de los condenados a muerte más violentos del mundo escapan para enfrentarlo.",
+    portada: "https://easycdn.es/1/series/p/baki_3625.jpg",
+    generos: ["Acción", "Artes Marciales", "Shounen"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK",
+            "Latino": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Bucchigiri"] = {
+    nombresAlternativos: {
+        "Japonés": "ぶっちぎり?!",
+        "Ingles": "Bucchigiri?!"
+    },
+    sinopsis: "Arajin Tomoshibi se ve envuelto en batallas entre delincuentes tras reencontrarse con un viejo amigo, todo mientras un genio gigante aparece para complicar las cosas.",
+    portada: "https://m.media-amazon.com/images/M/MV5BZmM2NDZjMWYtZTA1MS00MjUxLTk0ZjUtMGYzM2NmYzJlNDU5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    generos: ["Acción", "Fantasía", "Delincuentes"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK",
+            "Latino": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Bye Bye Earth"] = {
+    nombresAlternativos: {
+        "Japonés": "ばいばい、アース",
+        "Ingles": "Bye Bye, Earth"
+    },
+    sinopsis: "En un mundo donde todos tienen rasgos animales, Belle Lablac es la única que parece humana. Empuñando una espada gigante, viaja para descubrir su origen.",
+    portada: "https://a.storyblok.com/f/178900/960x1355/10a84fa91d/bye-bye-earth-key-visual.png/m/filters:quality(95)format(webp)",
+    generos: ["Acción", "Fantasía", "Aventura"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Bocchi the Rock"] = {
+    nombresAlternativos: {
+        "Japonés": "ぼっち・ざ・ろっく！",
+        "Ingles": "Bocchi the Rock!"
+    },
+    sinopsis: "Hitori Gotou es una chica extremadamente tímida que ama la guitarra. Su vida cambia cuando es invitada a unirse a una banda de rock.",
+    portada: "https://pbs.twimg.com/media/FmWuG60aUAYsS2w.jpg",
+    generos: ["Comedia", "Música", "Recuentos de la vida"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK",
+            "Latino": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Boukyaku Battery"] = {
+    nombresAlternativos: {
+        "Japonés": "忘却バッテリー",
+        "Ingles": "Oblivion Battery"
+    },
+    sinopsis: "Un famoso receptor de béisbol pierde la memoria y su habilidad, obligando a su compañero lanzador estrella a intentar reconstruir su legendaria batería desde cero.",
+    portada: "https://a.storyblok.com/f/178900/1064x1504/f2b6d57b62/boukyaku-battery-visual.jpg/m/filters:quality(95)format(webp)", // Pega aquí tu enlace de Google
+    generos: ["Deportes", "Comedia", "Escolar"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Bukiyou na Senpai"] = {
+    nombresAlternativos: {
+        "Japonés": "不器用な先輩.",
+        "Ingles": "My Awkward Senpai"
+    },
+    sinopsis: "Una comedia romántica de oficina centrada en una superior que es estricta y torpe para expresar sus verdaderos sentimientos hacia su subordinado.",
+    portada: "https://a.storyblok.com/f/178900/750x1059/eca370d8a1/my-awkward-senpai-teaser-visual.jpg/m/filters:quality(95)format(webp)",
+    generos: ["Romance", "Recuentos de la vida"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Boushoku no Berserk"] = {
+    nombresAlternativos: {
+        "Japonés": "暴食のベルセルク",
+        "Ingles": "Berserk of Gluttony"
+    },
+    sinopsis: "Fate Graphite, un joven con la habilidad aparentemente inútil de la Gula, descubre que puede absorber los poderes de quienes asesina, iniciando un camino de venganza.",
+    portada: "https://img.anmosugoi.com/file/media-sugoi/2022/10/Boushoku-no-Berserk-anime-NL-vol-1.jpg",
+    generos: ["Acción", "Fantasía", "Oscuro"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK",
+            "Latino": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Busu ni Hanataba wo"] = {
+    nombresAlternativos: {
+        "Japonés": "ブスに花束を。",
+        "Ingles": "Flowers to the Unattractive Girl"
+    },
+    sinopsis: "Tabata, una chica que se considera fea y solitaria, comienza una inesperada relación de amistad y romance con el chico más popular de su clase.",
+    portada: "https://instagram.flim38-1.fna.fbcdn.net/v/t39.30808-6/515374499_1062353839346750_5635906178725396746_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=101&ig_cache_key=MzY2OTUxOTcyMTEzMTA3OTY3Ng%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjg0OXgxMDYxLnNkci5DMyJ9&_nc_ohc=2I9y2SOw51sQ7kNvwGuvjdC&_nc_oc=AdkjvfoRiQBmKgTfCAQHkFVlhzovHooSUFE950O8QOj9uNqKEjUlOkhtuIyxsPG_GAU&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.flim38-1.fna&_nc_gid=1ZGf2BQ2etEarx05hZRejw&_nc_ss=8&oh=00_AfyaiF1aJuzWqNuoTVy13WXdjZHulshfB-ubA1AtVcWKiA&oe=69BFE13A",
+    generos: ["Romance", "Comedia", "Escolar"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Busamen Gachi Fighter"] = {
+    nombresAlternativos: {
+        "Japonés": "ブサメンガチファイター",
+        "Ingles": "The Ugly Guy Who Became a Top Fighter"
+    },
+    sinopsis: "Un hombre que es constantemente rechazado por su apariencia en el mundo real reencarna en un mundo de fantasía donde su 'fealdad' se convierte en su mayor ventaja de combate.",
+    portada: "https://scontent.flim38-1.fna.fbcdn.net/v/t39.30808-6/505220478_1019601140154410_5172779976284912164_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=13d280&_nc_eui2=AeGHSnFqvucVA29z4opIf7J0SVr9RiDXGLVJWv1GINcYtdSKDED4QilASiX-afBoFrKvCt5PyojYESW8v-ZrvmSl&_nc_ohc=ARKgZJI4b8oQ7kNvwEhTqLO&_nc_oc=AdmbZKG3xWcFp0EDfDIxzAt4qC-6TR6an1q148UEyAU62A-awU0mKul8230WnkLDz04&_nc_zt=23&_nc_ht=scontent.flim38-1.fna&_nc_gid=tmRFSoPVn3MqLH7hdtP_gQ&_nc_ss=8&oh=00_AfwbmfgOrKQBpuRjT4DZCuhNaJ5hPK-i4D6w0o8kgbNp7w&oe=69BFDBB9",
+    generos: ["Acción", "Fantasía", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Ballpark de Tsukamaete!"] = {
+    nombresAlternativos: {
+        "Japonés": "ボールパークでつかまえて！",
+        "Ingles": "The Diamond's Edge"
+    },
+    sinopsis: "Sigue las interacciones diarias entre los fanáticos, los jugadores y las trabajadoras de un estadio de béisbol, mezclando deporte con historias de vida.",
+    portada: "https://a.storyblok.com/f/178900/900x1200/282a25fe89/the-catcher-in-the-ballpark-visual.jpg/m/filters:quality(95)format(webp)",
+    generos: ["Deportes", "Comedia"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Bartender: Kami no Glass"] = {
+    nombresAlternativos: {
+        "Japonés": "バーテンダー 神のグラス",
+        "Ingles": "Bartender: Glass of God"
+    },
+    sinopsis: "Ryū Sasakura es un barman prodigio capaz de preparar el cóctel perfecto para consolar el alma de sus clientes, mezclando historias de vida con el arte de la coctelería.",
+    portada: "https://m.media-amazon.com/images/M/MV5BMWMxMmViZGUtNmVlMi00YTVkLTgxOTMtZmFiMjBmN2E5N2Y4XkEyXkFqcGc@._V1_.jpg", // Pega aquí tu enlace de Google
+    generos: ["Drama", "Recuentos de la vida"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Babanbabanban Vampire"] = {
+    nombresAlternativos: {
+        "Japonés": "ババンババンバンバンパイア",
+        "Ingles": "Babanbabanban Vampire"
+    },
+    sinopsis: "Mori, un vampiro de 450 años, trabaja en una casa de baños tradicional mientras espera que su joven protegido alcance la madurez para probar su 'sangre virgen'.",
+    portada: "https://a.storyblok.com/f/178900/750x1058/fdb6b8300f/babanbabanban_vampire_key_art.jpg/m/filters:quality(95)format(webp)",
+    generos: ["Comedia", "Sobrenatural"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Blue Archive the Animation"] = {
+    nombresAlternativos: {
+        "Japonés": "ブルーアーカイブ The Animation",
+        "Ingles": "Blue Archive The Animation"
+    },
+    sinopsis: "En la ciudad de Kivotos, las estudiantes de diversas academias armadas enfrentan crisis diarias con la ayuda del 'Sensei' para mantener la paz y el orden.",
+    portada: "https://a.storyblok.com/f/178900/960x1357/27749564a2/blue-archive-the-animation-key-visual.jpg/m/filters:quality(95)format(webp)",
+    generos: ["Acción", "Ciencia Ficción", "Escolar"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Bokura no Ameiro Protocol"] = {
+    nombresAlternativos: {
+        "Japonés": "僕らの雨いろプロトコル",
+        "Ingles": "Our Rainy Protocol"
+    },
+    sinopsis: "Shun Tokinoya se adentra en el mundo de los eSports competitivos para salvar el centro de juegos de su familia y superar un trauma del pasado.",
+    portada: "https://m.media-amazon.com/images/M/MV5BNDA2ZWU2NzYtMDVlOS00YmZkLTk4MGEtMTA5ZWZkOGRkMzJlXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    generos: ["Deportes", "Drama"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["BanG Dream! It's MyGO!!!!!"] = {
+    nombresAlternativos: {
+        "Japonés": "BanG Dream! It's MyGO!!!!!",
+        "Ingles": "BanG Dream! It's MyGO!!!!!"
+    },
+    sinopsis: "Tras la disolución de su banda anterior, Tomori busca formar un nuevo grupo musical, navegando por las complejas emociones y amistades de la adolescencia.",
+    portada: "https://pics.filmaffinity.com/BanG_Dream_It_s_MyGO_Serie_de_TV-530050095-large.jpg",
+    generos: ["Música", "Drama"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Bleach: Sennen Kessen-hen"] = {
+    nombresAlternativos: {
+        "Japonés": "BLEACH 千年血戦篇",
+        "Ingles": "Bleach: Thousand-Year Blood War"
+    },
+    sinopsis: "La Sociedad de Almas se enfrenta a la amenaza definitiva: el regreso del Quincy Rey Yhwach y su ejército, el Wandenreich, en una guerra épica por la supervivencia.",
+    portada: "https://m.media-amazon.com/images/M/MV5BMjgyM2QzMjAtOGZjOS00OGFkLTkxZGYtMDJjZGM5MzIzYmM3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    generos: ["Acción", "Aventura", "Sobrenatural", "Shounen"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK",
+            "Latino": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Buta no Liver wa Kanetsu Shiro"] = {
+    nombresAlternativos: {
+        "Japonés": "豚のレバーは加熱しろ",
+        "Ingles": "Butareba -The Story of a Man Who Turned into a Pig-"
+    },
+    sinopsis: "Un otaku come hígado de cerdo crudo y reencarna en un cerdo en un mundo de fantasía. Allí conoce a Jess, una chica que puede leer la mente y con quien vivirá grandes aventuras.",
+    portada: "https://m.media-amazon.com/images/M/MV5BZmRmZmFmMDctYTI2Zi00M2JjLWJlMzQtYWFhYWI2ZWU5Zjg4XkEyXkFqcGc@._V1_.jpg", // Pega aquí tu enlace de Google
+    generos: ["Fantasía", "Aventura", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Bloody Escape: Bats out of Hell"] = {
+    nombresAlternativos: {
+        "Japonés": "BLOODY ESCAPE -地獄の逃走劇-",
+        "Ingles": "Bloody Escape"
+    },
+    sinopsis: "Kisaragi es un cyborg perseguido por una organización de vampiros que desea conquistar Tokio. Una historia de acción frenética y supervivencia en un futuro distópico.",
+    portada: "https://m.media-amazon.com/images/S/pv-target-images/2e2f679543a930a50019047579f027a4335abbf65a44662a43f3b32c11d0d04b.jpg",
+    generos: ["Acción", "Ciencia Ficción", "Vampiros"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Benriya Saitou-san, Isekai ni Iku"] = {
+    nombresAlternativos: {
+        "Japonés": "便利屋斎藤さん、異世界に行く",
+        "Ingles": "Handyman Saitou in Another World"
+    },
+    sinopsis: "Saitou es un manitas ordinario que reencarna en otro mundo. Sus habilidades prácticas para abrir cerraduras y reparar equipos lo vuelven indispensable para su grupo de aventureros.",
+    portada: "https://image.tmdb.org/t/p/original/pVmlQMTvwuUfwMmsCGb4X2RafKL.jpg",
+    generos: ["Aventura", "Comedia", "Fantasía", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK",
+            "Latino": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Boku no Kokoro no Yabai Yatsu"] = {
+    nombresAlternativos: {
+        "Japonés": "僕の心のヤバイやつ",
+        "Ingles": "The Dangers in My Heart"
+    },
+    sinopsis: "Kyotaro Ichikawa es un chico solitario con fantasías oscuras que termina fascinado por Anna Yamada, la chica más popular y alegre de su clase.",
+    portada: "https://m.media-amazon.com/images/M/MV5BMjYwMDMwNDQtZTdmNy00YzBkLTliMzYtNzkzNTc1Njc1OWI2XkEyXkFqcGc@._V1_.jpg",
+    generos: ["Romance", "Comedia", "Escolar"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        },
+        "Temporada 2":{
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Boku no Tsuma wa Kanjou ga Nai"] = {
+    nombresAlternativos: {
+        "Japonés": "僕の妻は感情がない",
+        "Ingles": "My Wife Has No Emotion"
+    },
+    sinopsis: "Takuma compra una robot doméstica llamada Mina. Aunque ella es una máquina, él comienza a tratarla como si fuera su esposa, explorando lo que significa el amor.",
+    portada: "https://m.media-amazon.com/images/M/MV5BYWFkMTkwYWMtMTNiMy00Yzg0LThiMTctYzI2NGMyZGY5Y2FkXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    generos: ["Ciencia Ficción", "Romance", "Recuentos de la vida"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
+
+window.base["Boukensha ni Naritai to Miyako ni Deteitta Musume ga"] = {
+    nombresAlternativos: {
+        "Japonés": "冒険者になりたいと都に出て行った娘がSランクになってた",
+        "Ingles": "My Daughter Left the Home and Returned as an S-Rank Adventurer"
+    },
+    sinopsis: "Belgrieve es un aventurero retirado que crió a una hija adoptiva, Angeline. Años después, ella regresa convertida en una guerrera de rango S de fama mundial.",
+    portada: "https://scontent.flim38-1.fna.fbcdn.net/v/t39.30808-6/490469229_1252359436578879_4147787002620433280_n.jpg?stp=dst-jpg_s720x720_tt6&_nc_cat=105&ccb=1-7&_nc_sid=7b2446&_nc_eui2=AeHxIQEdOm4FezH1DNvKEwYWkg80VhD3lDaSDzRWEPeUNhpJpxWQK9LsTuofYxBnzO4Op_Nz2BwevEHvNW64pobG&_nc_ohc=Iu7OUZXqPeoQ7kNvwGwnRLJ&_nc_oc=AdkQdUa5C0hv4wSuOWWo0oRbUOlP_dDR6IYYl6T6apCp3ua0iZ4gDZehmbh6pmG3vrY&_nc_zt=23&_nc_ht=scontent.flim38-1.fna&_nc_gid=UNfmtkyRSJ_m-VTFmK1Pgg&_nc_ss=8&oh=00_AfzmmZjQb4hhQK7dPtFeH1wxJl2WR6NCDUPRZTqndcmslw&oe=69BFD7C5",
+    generos: ["Acción", "Fantasía", "Aventura"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/TuCanal/LINK"
+        }
+    }
+};
