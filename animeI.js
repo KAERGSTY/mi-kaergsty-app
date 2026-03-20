404cc3767b7f84f91a0a852d417e49e92801c55c
KAERGSTY <137520623+KAERGSTY@users.noreply.github.com>
2026-03-20 10:35:44 -0500

    I

---
+++ animeI.js
@@ -0,0 +1,242 @@
+// Base de Datos - Letra I
+window.base["Ishura"] = {
+    carpeta: "imgI",
+    nombresAlternativos: {
+        "Japonés": "異修羅",
+        "Ingles": "Ishura"
+    },
+    sinopsis: "En un mundo donde el Rey Demonio ha muerto, una horda de semidioses capaces de derrotarlo ha heredado el mundo. Un esgrimista maestro, un lancero veloz y otros guerreros compiten por el título de 'Verdadero Héroe'.",
+    generos: ["Acción", "Fantasía", "Aventura"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/12810"
+        }
+    }
+};
+
+window.base["Iseleve"] = {
+    carpeta: "imgI",
+    nombresAlternativos: {
+        "Japonés": "異世界でチート能力を手にした俺は、現実世界をも無双する ～レベルアップは人生を変えた～",
+        "Ingles": "I Got a Cheat Skill in Another World and Became Unrivaled in the Real World, Too"
+    },
+    sinopsis: "Yuuya Tenjou es un chico acosado que descubre un portal a otro mundo en la casa de su abuelo. Al subir de nivel en el otro mundo, su cuerpo y habilidades en el mundo real cambian drásticamente.",
+    generos: ["Acción", "Aventura", "Fantasía", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/660",
+            "Latino": "https://t.me/Kaergsty/2154/2312"
+        }
+    }
+};
+
+window.base["Isekai Shikkaku"] = {
+    carpeta: "imgI",
+    nombresAlternativos: {
+        "Japonés": "異世界失格",
+        "Ingles": "No Longer Allowed in Another World"
+    },
+    sinopsis: "Un famoso escritor decide suicidarse junto a su amada, pero termina siendo transportado a otro mundo. A diferencia de otros héroes, él no tiene interés en poderes, solo busca un lugar digno para morir.",
+    generos: ["Aventura", "Comedia", "Fantasía", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/23974"
+        }
+    }
+};
+
+window.base["Isekai Suicide Squad"] = {
+    carpeta: "imgI",
+    nombresAlternativos: {
+        "Japonés": "異世界スーサイド・スクワッド",
+        "Ingles": "Suicide Squad ISEKAI"
+    },
+    sinopsis: "Harley Quinn, Deadshot y el Escuadrón Suicida son enviados por Amanda Waller a un mundo de espadas y magia. Si no conquistan este reino en 72 horas, las bombas en sus cuellos explotarán.",
+    generos: ["Acción", "Fantasía", "Aventura"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/23013"
+        }
+    }
+};
+
+window.base["Ishuzoku Reviewers"] = {
+    carpeta: "imgI",
+    nombresAlternativos: {
+        "Japonés": "異種族レビュアーズ",
+        "Ingles": "Interspecies Reviewers"
+    },
+    sinopsis: "En un mundo fantástico lleno de diversas razas, un grupo de guerreros se dedica a visitar y calificar los servicios de los burdeles de distintas especies para publicar sus reseñas.",
+    generos: ["Comedia", "Ecchi", "Fantasía"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/7629"
+        }
+    }
+};
+
+window.base["Isekai Nonbiri Nouka"] = {
+    carpeta: "imgI",
+    nombresAlternativos: {
+        "Japonés": "異世界んびり農家",
+        "Ingles": "Farming Life in Another World"
+    },
+    sinopsis: "Hiraku muere por una enfermedad y es revivido en otro mundo con un cuerpo sano y una herramienta agrícola divina. Su objetivo: disfrutar de una vida tranquila como granjero.",
+    generos: ["Fantasía", "Harem", "Recuentos de la vida", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/645"
+        }
+    }
+};
+
+window.base["Isekai One Turn Kill Neesan"] = {
+    carpeta: "imgI",
+    nombresAlternativos: {
+        "Japonés": "異世界ワンターンキル姉さん ～姉同伴の異世界生活はじめました～",
+        "Ingles": "My One-Hit Kill Sister"
+    },
+    sinopsis: "Asahi es transportado a otro mundo pero es muy débil. Sin embargo, su hermana mayor Maya también llega y resulta tener las habilidades más rotas del mundo, protegiéndolo siempre.",
+    generos: ["Comedia", "Fantasía", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/646"
+        }
+    }
+};
+
+window.base["Isekai Meikyuu de Harem wo"] = {
+    carpeta: "imgI",
+    nombresAlternativos: {
+        "Japonés": "異世界迷宮でハーレムを",
+        "Ingles": "Harem in the Labyrinth of Another World"
+    },
+    sinopsis: "Michio Kaga utiliza un sitio web extraño que lo transporta a un mundo de videojuegos. Allí, decide usar sus nuevas habilidades para obtener dinero, fama y formar su propio harem.",
+    generos: ["Acción", "Aventura", "Ecchi", "Harem", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/562"
+        }
+    }
+};
+
+window.base["Izure Saikyou no Renkinjutsushi"] = {
+    carpeta: "imgI",
+    nombresAlternativos: {
+        "Japonés": "いずれ最強の錬金術師？",
+        "Ingles": "Possibly the Greatest Alchemist of All Time?"
+    },
+    sinopsis: "Iruma es transportado a otro mundo por un error divino. Como compensación, recibe la habilidad de alquimia, la cual empezará a desarrollar hasta niveles insospechados.",
+    generos: ["Aventura", "Fantasía", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/34764"
+        }
+    }
+};
+
+window.base["Isekai Shoukan wa Nidome desu"] = {
+    carpeta: "imgI",
+    nombresAlternativos: {
+        "Japonés": "異世界召喚は二度目です",
+        "Ingles": "Summoned to Another World for a Second Time"
+    },
+    sinopsis: "Un hombre que ya salvó un mundo fantástico una vez es invocado de nuevo al mismo lugar años después, pero ahora con el cuerpo de un estudiante de secundaria.",
+    generos: ["Acción", "Aventura", "Comedia", "Harem", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/647"
+        }
+    }
+};
+
+window.base["Isekai wa Smartphone to tomo ni"] = {
+    carpeta: "imgI",
+    nombresAlternativos: {
+        "Japonés": "異世界はスマートフォンとともに。2",
+        "Ingles": "In Another World With My Smartphone 2"
+    },
+    sinopsis: "Touya continúa su vida en el mundo fantástico con su smartphone y sus crecientes poderes mágicos, mientras su grupo de prometidas sigue aumentando.",
+    generos: ["Aventura", "Comedia", "Harem", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 2": {
+            "Japonés": "https://t.me/Kaergsty/2/818"
+        }
+    }
+};
+
+window.base["Isekai de Mofumofu Nadenade"] = {
+    carpeta: "imgI",
+    nombresAlternativos: {
+        "Japonés": "異世界でもふもふなでなでするためにがんばってます。",
+        "Ingles": "Fluffy Paradise"
+    },
+    sinopsis: "Akira reencarna como la pequeña Nefertima con un don especial: ser amada por todos los seres vivos que no sean humanos. ¡Su misión es acariciar a todas las criaturas mágicas posibles!",
+    generos: ["Aventura", "Fantasía", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/12626"
+        }
+    }
+};
+
+window.base["Isekai Yururi Kikou"] = {
+    carpeta: "imgI",
+    nombresAlternativos: {
+        "Japonés": "異世界ゆるり紀行 ～子育てしながら冒険者します～",
+        "Ingles": "A Journey Through Another World: Raising Kids While Adventuring"
+    },
+    sinopsis: "Takumi es transportado a otro mundo donde encuentra a dos niños gemelos en un bosque peligroso. Decide adoptarlos y cuidarlos mientras vive aventuras como aventurero.",
+    generos: ["Aventura", "Fantasía", "Recuentos de la vida", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/23833",
+            "Latino": "https://t.me/Kaergsty/2/23833"
+        }
+    }
+};
+
+window.base["Isekai Mokushiroku Mynoghra"] = {
+    carpeta: "imgI",
+    nombresAlternativos: {
+        "Japonés": "異世界黙示録マイノグラ～破滅の文明で始める世界征服～",
+        "Ingles": "Isekai Apocalypse Mynoghra"
+    },
+    sinopsis: "Ira Takuto reencarna en un mundo similar a su juego de estrategia favorito como el Dios de una civilización maligna. Junto a su heroína favorita, decidirá conquistar el mundo.",
+    generos: ["Fantasía", "Estrategia", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/49320"
+        }
+    }
+};
+
+window.base["Isshun de Chiryou shiteita noni"] = {
+    carpeta: "imgI",
+    nombresAlternativos: {
+        "Japonés": "一瞬で治療していたのに役立たずと追放された天才治癒師、闇ヒーラーとして楽しく生きる",
+        "Ingles": "The Genius Healer Who Was Banished"
+    },
+    sinopsis: "Zenos, un sanador capaz de curar heridas al instante, es expulsado de su grupo por ser 'inútil'. Ahora, decide abrir su propia clínica clandestina como sanador oscuro.",
+    generos: ["Fantasía", "Aventura"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/43347"
+        }
+    }
+};
