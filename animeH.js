3ad648e8a6b8766348a0a78d420b1fe84faa5c29
KAERGSTY <137520623+KAERGSTY@users.noreply.github.com>
2026-03-20 10:06:09 -0500

    M

---
+++ animeH.js
@@ -0,0 +1,402 @@
+ // Base de Datos - Letra H
+window.base["Helck"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "ヘルク",
+        "Ingles": "Helck"
+    },
+    sinopsis: "Tras la derrota del Rey Demonio, el Reino de los Demonios organiza un torneo para elegir al próximo sucesor. El favorito es Helck, un héroe humano que afirma odiar a su propia especie.",
+    generos: ["Acción", "Aventura", "Fantasía"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/8079"
+        }
+    }
+};
+
+window.base["Hyouka"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "氷菓",
+        "Ingles": "Hyouka"
+    },
+    sinopsis: "Oreki Hotaro es un estudiante minimalista que termina uniéndose al Club de Literatura Clásica. Junto a Eru Chitanda, resolverá diversos misterios cotidianos que ocurren en su preparatoria.",
+    generos: ["Misterio", "Recuentos de la vida", "Escolar"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/34544"
+        }
+    }
+};
+
+window.base["Hundred"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "ハンドレッド",
+        "Ingles": "Hundred"
+    },
+    sinopsis: "Para combatir a los 'Savage', una forma de vida misteriosa, la humanidad utiliza el 'Hundred', la única arma capaz de enfrentarlos. Hayato Kisaragi busca convertirse en un maestro de esta arma.",
+    generos: ["Acción", "Mecha", "Ecchi", "Harem"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/13323"
+        }
+    }
+};
+
+window.base["Haigakura"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "ハイガクラ",
+        "Ingles": "Haigakura"
+    },
+    sinopsis: "En un mundo sostenido por deidades, Ichiyou busca capturar a los dioses que han escapado para restaurar el equilibrio de las tierras sagradas.",
+    generos: ["Fantasía", "Aventura"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/28981"
+        }
+    }
+};
+
+window.base["Hotel Inhumans"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "ホテル・インヒューマンズ",
+        "Ingles": "Hotel Inhumans"
+    },
+    sinopsis: "Dos asesinos a sueldo gestionan un hotel de lujo que sirve como refugio y lugar de descanso para otros criminales profesionales bajo reglas estrictas.",
+    generos: ["Acción", "Drama", "Seinen"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/49329"
+        }
+    }
+};
+
+window.base["Horimiya Piece"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "ホリミヤ -piece-",
+        "Ingles": "Horimiya: The Missing Pieces"
+    },
+    sinopsis: "Historias adicionales que no fueron adaptadas en la serie original, mostrando más momentos de la vida diaria de Hori, Miyamura y sus amigos.",
+    generos: ["Romance", "Recuentos de la vida", "Escolar"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/7898",
+            "Latino": "https://t.me/Kaergsty/2154/12358"
+        }
+    }
+};
+
+window.base["Highspeed Etoile"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "ハイスピード エトワール",
+        "Ingles": "Highspeed Etoile"
+    },
+    sinopsis: "En un futuro cercano donde los vehículos pueden alcanzar los 500 km/h de forma segura, Rin Rindo debuta en el mundo de las carreras de próxima generación conocida como NEX Race.",
+    generos: ["Carreras", "Ciencia Ficción", "Deportes"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/17431"
+        }
+    }
+};
+
+window.base["Harukana Receive"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "はるかなレシーブ",
+        "Ingles": "Harukana Receive"
+    },
+    sinopsis: "Haruka se muda a Okinawa y descubre su pasión por el vóley playa. Junto a su prima Kanata, formará un dúo para competir en torneos locales.",
+    generos: ["Deportes", "Recuentos de la vida", "Ecchi"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/4627"
+        }
+    }
+};
+
+window.base["Hoshikuzu Telepath"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "星屑テレパス",
+        "Ingles": "Stardust Telepath"
+    },
+    sinopsis: "Umika es una chica extremadamente tímida que conoce a Yuu, una estudiante de intercambio que afirma ser una alienígena con la capacidad de leer mentes al tocar frentes.",
+    generos: ["Ciencia Ficción", "Recuentos de la vida", "Escolar"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/8838"
+        }
+    }
+};
+
+window.base["Honey Lemon Soda"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "ハニーレモンソーダ",
+        "Ingles": "Honey Lemon Soda"
+    },
+    sinopsis: "Uka Ishimori es una chica retraída que desea cambiar su vida en la preparatoria. Allí conoce a Kai Miura, un chico de pelo rubio limón que la ayudará a salir de su caparazón.",
+    generos: ["Romance", "Escolar", "Shoujo"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/34773"
+        }
+    }
+};
+
+window.base["Hametsu no Oukoku"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "はめつのおうこく",
+        "Ingles": "The Kingdoms of Ruin"
+    },
+    sinopsis: "En un mundo donde la ciencia ha reemplazado a la magia, las brujas son perseguidas. Adonis, un humano criado por una bruja, jura venganza contra el imperio que destruyó todo lo que amaba.",
+    generos: ["Acción", "Fantasía", "Drama"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/8700"
+        }
+    }
+};
+
+window.base["Henjin no Salad Bowl"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "変人のサラダボウル",
+        "Ingles": "A Salad Bowl of Eccentrics"
+    },
+    sinopsis: "Sosuke Kaburaya es un detective pobre que conoce a Sara, una princesa de otro mundo con poderes mágicos. Juntos empiezan una extraña convivencia llena de personajes peculiares.",
+    generos: ["Comedia", "Fantasía", "Recuentos de la vida"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/17387"
+        }
+    }
+};
+
+window.base["Hikaru ga Shinda Natsu"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "光が死んだ夏",
+        "Ingles": "The Summer Hikaru Died"
+    },
+    sinopsis: "Yoshiki descubre que su mejor amigo, Hikaru, ha sido reemplazado por una entidad desconocida tras desaparecer en la montaña. A pesar de saberlo, Yoshiki decide seguir a su lado.",
+    generos: ["Misterio", "Horror", "Sobrenatural"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/49129"
+        }
+    }
+};
+
+window.base["Hai to Gensou no Grimgar"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "灰と幻想のグリムガル",
+        "Ingles": "Grimgar: Ashes and Illusions"
+    },
+    sinopsis: "Un grupo de desconocidos despierta en un mundo de fantasía sin recuerdos de su pasado. Para sobrevivir, deben convertirse en aprendices de soldados y enfrentar la dura realidad de la vida y la muerte.",
+    generos: ["Aventura", "Fantasía", "Drama", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/49532"
+        }
+    }
+};
+
+window.base["Hazure Skill Kinomi Master"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "外れスキル『木の実マスター』 〜スキルの実（たべたら死ぬ）を無限に食べられるようになった件について〜",
+        "Ingles": "The Failed Skill 'Nut Master'"
+    },
+    sinopsis: "Light posee la habilidad 'Maestro de las Nueces', considerada inútil hasta que descubre que puede comer las frutas de habilidad (que normalmente matan al usuario) de forma ilimitada.",
+    generos: ["Acción", "Aventura", "Fantasía"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/34778"
+        }
+    }
+};
+
+window.base["Hoshifuru Oukoku no Nina"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "星降る王国のニナ",
+        "Ingles": "Nina the Starry Bride"
+    },
+    sinopsis: "Nina, una joven huérfana con ojos de color azul lapislázuli, es obligada a hacerse pasar por la difunta princesa Alisha para salvar al reino a través de un matrimonio político.",
+    generos: ["Fantasía", "Romance", "Josei"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/28977",
+            "Latino": "https://t.me/Kaergsty/2/17387"
+        }
+    }
+};
+
+window.base["Hataraku Maou-sama S2"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "はたらく魔王さま！！",
+        "Ingles": "The Devil is a Part-Timer! Season 2"
+    },
+    sinopsis: "Sadao Maou continúa su vida como empleado de comida rápida en el Tokio moderno, enfrentando nuevos retos celestiales y la aparición de una niña misteriosa llamada Alas Ramus.",
+    generos: ["Comedia", "Fantasía", "Recuentos de la vida"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 2": {
+            "Japonés": "https://t.me/Kaergsty/2/8219"
+        }
+    }
+};
+
+window.base["Hananoi-kun to Koi no Yamai"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "花野井くんと恋の病",
+        "Ingles": "A Condition Called Love"
+    },
+    sinopsis: "Hotaru es una chica que no entiende el amor, hasta que conoce a Hananoi-kun, un chico cuya devoción y formas intensas de expresar sus sentimientos empezarán a cambiar su perspectiva.",
+    generos: ["Romance", "Escolar", "Shoujo"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/17369"
+        }
+    }
+};
+
+window.base["Haite Kudasai Takamine-san"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "履いてください、鷹峰さん",
+        "Ingles": "Please Put Them On, Takamine-san"
+    },
+    sinopsis: "Takamine es la presidenta del consejo estudiantil y una genio, pero tiene un secreto: su poder depende de que no lleve ropa interior. Kousaka termina convirtiéndose en su 'guardarropa' personal.",
+    generos: ["Comedia", "Romance", "Escolar", "Ecchi"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/43794"
+        }
+    }
+};
+
+window.base["Hibi wa Yogiredo Meshi Umashi"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "日々は過ぎれど飯うまし",
+        "Ingles": "Days Pass, but the Food is Delicious"
+    },
+    sinopsis: "Un anime centrado en el placer de comer y cocinar, mostrando cómo una buena comida puede mejorar incluso los días más rutinarios.",
+    generos: ["Recuentos de la vida", "Gastronomía"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/43339"
+        }
+    }
+};
+
+window.base["Hitoribocchi no Isekai Kouryaku"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "ひとりぼっちの異世界攻略",
+        "Ingles": "Loner Life in Another World"
+    },
+    sinopsis: "Haruka es transportado a otro mundo junto a su clase, pero al llegar tarde al reparto de habilidades, se queda con todas las habilidades 'sobrantes' y debe sobrevivir solo.",
+    generos: ["Acción", "Aventura", "Fantasía", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/28367"
+        }
+    }
+};
+
+window.base["Hana wa Saku Shura no Gotoku"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "花は咲く、修羅の如く",
+        "Ingles": "Flowers Bloom, Like Asura"
+    },
+    sinopsis: "Hana es una chica que ama leer en voz alta. Al unirse al club de radiodifusión de su escuela, descubrirá un nuevo mundo de expresión y competencia.",
+    generos: ["Drama", "Recuentos de la vida", "Escolar"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/36194"
+        }
+    }
+};
+
+window.base["Himesama Goumon no Jikan desu"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "姫様“拷問”の時間です",
+        "Ingles": "'Tis Time for 'Torture,' Princess"
+    },
+    sinopsis: "Una princesa capturada por el ejército demoníaco debe enfrentar 'torturas' diarias que consisten en deliciosas comidas y tentaciones humanas imposibles de resistir.",
+    generos: ["Comedia", "Fantasía", "Gastronomía"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/13046"
+        }
+    }
+};
+
+window.base["Hikikomari Kyuuketsuki no Monmon"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "ひきこまり吸血姫の悶々",
+        "Ingles": "The Vexations of a Shut-In Vampire Princess"
+    },
+    sinopsis: "Terakomari, una vampiresa que odia la sangre y es una hikikomori, es nombrada comandante de un ejército a pesar de su debilidad física.",
+    generos: ["Acción", "Comedia", "Fantasía"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/8761"
+        }
+    }
+};
+
+window.base["Hazurewaku no Joutai Ijou Skill"] = {
+    carpeta: "imgH",
+    nombresAlternativos: {
+        "Japonés": "ハズレ枠の【状態異常スキル】で最強になった俺がすべてを蹂躙するまで",
+        "Ingles": "Failure Frame: I Became the Strongest and Annihilated Everything with Low-Level Spells"
+    },
+    sinopsis: "Touka Mimori es descartado por una diosa tras recibir una habilidad de 'estado alterado' de bajo nivel. Sin embargo, descubre que su poder es absoluto contra cualquier enemigo.",
+    generos: ["Acción", "Aventura", "Fantasía", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/23493"
+        }
+    }
+};
