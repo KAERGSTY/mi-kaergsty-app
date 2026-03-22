a59db6f462fdb36f8c4c4481da9a006cbd89b3cf
KAERGSTY <137520623+KAERGSTY@users.noreply.github.com>
2026-03-22 03:15:51 -0500

    N

---
+++ animeN.js
@@ -0,0 +1,239 @@
+window.base["NegaPosi Angler"] = {
+    carpeta: "imgN",
+    nombresAlternativos: {
+        "Japonés": "ネガポジアングラー",
+        "Ingles": "Negative Positive Angler"
+    },
+    sinopsis: "Tsunehiro Sasaki, un estudiante universitario con una deuda enorme y a quien le han diagnosticado poco tiempo de vida, cae al mar y es rescatado por un grupo de entusiastas de la pesca.",
+    generos: ["Recuentos de la vida", "Deportes", "Drama"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/28759"
+        }
+    }
+};
+
+window.base["Ninja to Gokudou"] = {
+    carpeta: "imgN",
+    nombresAlternativos: {
+        "Japonés": "忍者と極道",
+        "Ingles": "Ninja and Gokudo"
+    },
+    sinopsis: "Dos jóvenes, uno un ninja que opera en las sombras y el otro un líder de la yakuza, entablan una amistad sin conocer sus verdaderas identidades en una Tokyo plagada de violencia.",
+    generos: ["Acción", "Artes Marciales", "Seinen"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/54133"
+        }
+    }
+};
+
+window.base["Na Nare Hana Nare"] = {
+    carpeta: "imgN",
+    nombresAlternativos: {
+        "Japonés": "菜なれ花なれ",
+        "Ingles": "Narenare -Cheer for You!-"
+    },
+    sinopsis: "Seis chicas de secundaria con diferentes talentos y personalidades se unen para formar un grupo de apoyo y cheerleading, encontrando el camino hacia sus sueños.",
+    generos: ["Recuentos de la vida", "Música"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/23832"
+        }
+    }
+};
+
+window.base["Nukitashi the Animation"] = {
+    carpeta: "imgN",
+    nombresAlternativos: {
+        "Japonés": "抜きゲーみたいな島に住んでる貧乳 (ぬきたし)",
+        "Ingles": "Nukitashi the Animation"
+    },
+    sinopsis: "En una isla donde las leyes sexuales son extremadamente liberales y obligatorias, un grupo de estudiantes intenta rebelarse contra el sistema establecido.",
+    generos: ["Comedia", "Echi", "Parodia"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/49511"
+        }
+    }
+};
+
+window.base["NieR Automata Ver1-1"] = {
+    carpeta: "imgN",
+    nombresAlternativos: {
+        "Japonés": "ニーア オートマタ Ver1.1a",
+        "Ingles": "NieR:Automata Ver1.1a"
+    },
+    sinopsis: "La historia de los androides 2B, 9S y A2 y su batalla para recuperar la distopía impulsada por máquinas que ha sido invadida por máquinas poderosas.",
+    generos: ["Acción", "Ciencia Ficción", "Fantasía"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/24779"
+        }
+    }
+};
+
+window.base["Nige Jouzu no Wakagimi"] = {
+    carpeta: "imgN",
+    nombresAlternativos: {
+        "Japonés": "逃げ上手の若君",
+        "Ingles": "The Elusive Samurai"
+    },
+    sinopsis: "Tras la caída del shogunato Kamakura, el joven heredero Tokiyuki Hojo debe sobrevivir huyendo y escondiéndose para algún día recuperar su honor.",
+    generos: ["Histórico", "Acción", "Aventura"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/23647"
+        }
+    }
+};
+
+window.base["Nihon e Youkoso Elf-san"] = {
+    sinopsis: "Kazuhiro es un oficinista que viaja a un mundo de fantasía en sus sueños. Un día, al despertar, descubre que la elfa que lo acompañaba ha regresado con él al Japón moderno.",
+    carpeta: "imgN",
+    nombresAlternativos: {
+        "Japonés": "日本へようこそエルフさん。",
+        "Ingles": "Welcome to Japan, Ms. Elf!"
+    },
+    generos: ["Fantasía", "Romance", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/34759"
+        }
+    }
+};
+
+window.base["Nyaight of the Living Cat"] = {
+    carpeta: "imgN",
+    nombresAlternativos: {
+        "Japonés": "ニャイト・オブ・ザ・リビングキャット",
+        "Ingles": "Nyaight of the Living Cat"
+    },
+    sinopsis: "Un virus convierte a los humanos en gatos. En este mundo post-apocalíptico, los supervivientes deben resistir la tentación de acariciar a los felinos para no transformarse.",
+    generos: ["Comedia", "Sobrenatural", "Parodia"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/49321"
+        }
+    }
+};
+
+window.base["Natsume Yuujinchou Shichi"] = {
+    carpeta: "imgN",
+    nombresAlternativos: {
+        "Japonés": "夏目友人帳 漆",
+        "Ingles": "Natsume's Book of Friends Season 7"
+    },
+    sinopsis: "Séptima temporada de las vivencias de Takashi Natsume, un joven capaz de ver espíritus que heredó un libro lleno de contratos de nombres de yokais.",
+    generos: ["Recuentos de la vida", "Sobrenatural", "Drama"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/28982"
+        }
+    }
+};
+
+window.base["Nazotoki wa Dinner no Ato de"] = {
+    carpeta: "imgN",
+    nombresAlternativos: {
+        "Japonés": "謎解きはディナーのあとで",
+        "Ingles": "The After-Dinner Mysteries"
+    },
+    sinopsis: "Reiko Hosho es una rica heredera y detective novata que resuelve crímenes con la ayuda de su mordaz mayordomo, Kageyama, quien tiene un ingenio superior.",
+    generos: ["Misterio", "Comedia"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/46445"
+        }
+    }
+};
+
+window.base["Nozomanu Fushi no Boukensha"] = {
+    carpeta: "imgN",
+    nombresAlternativos: {
+        "Japonés": "望まぬ不死の冒険者",
+        "Ingles": "The Unwanted Undead Adventurer"
+    },
+    sinopsis: "Rentt Faina, un aventurero de clase baja, muere a manos de un dragón pero despierta como un esqueleto. Ahora busca evolucionar para recuperar su forma humana.",
+    generos: ["Aventura", "Fantasía", "Acción"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/12915"
+        }
+    }
+};
+
+window.base["Ninja to Koroshiya no Futarigurashi"] = {
+    carpeta: "imgN",
+    nombresAlternativos: {
+        "Japonés": "忍者と殺し屋のふたりぐらし",
+        "Ingles": "The Life of a Ninja and an Assassin Living Together"
+    },
+    sinopsis: "La comedia diaria de una ninja y una asesina profesional que terminan compartiendo apartamento mientras intentan llevar una vida normal.",
+    generos: ["Comedia", "Recuentos de la vida"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/43334"
+        }
+    }
+};
+
+window.base["Nekuronomiko no Kozumikku Hora Sho"] = {
+    carpeta: "imgN",
+    nombresAlternativos: {
+        "Japonés": "ネクロノミコのコズミックホラー笑",
+        "Ingles": "Necronomiko's Cosmic Horror Show"
+    },
+    sinopsis: "Historias cortas basadas en el horror cósmico de Lovecraft pero con un giro cómico y disparatado protagonizado por chicas anime.",
+    generos: ["Comedia", "Terror", "Parodia"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/48912"
+        }
+    }
+};
+
+window.base["NEET Kunoichi to Nazeka Dousei Hajimemashita"] = {
+    carpeta: "imgN",
+    nombresAlternativos: {
+        "Japonés": "ニートくノ一となぜか同棲はじめました",
+        "Ingles": "I Somehow Started Living With a NEET Kunoichi"
+    },
+    sinopsis: "Un oficinista termina viviendo con una ninja genio que, lamentablemente, es una completa NEET adicta a los videojuegos y la comida basura.",
+    generos: ["Comedia", "Romance", "Echi"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/36065"
+        }
+    }
+};
+
+window.base["Naze Boku no Sekai wo Daremo Oboeteinai no ka"] = {
+    carpeta: "imgN",
+    nombresAlternativos: {
+        "Japonés": "なぜ僕の世界を誰も覚えていないのか？",
+        "Ingles": "Why Does Nobody Remember My World?"
+    },
+    sinopsis: "Kai vive en un mundo donde la humanidad ganó la gran guerra. De repente, el mundo se 'reescribe' y ahora la humanidad perdió, y nadie recuerda a Kai excepto él mismo.",
+    generos: ["Acción", "Fantasía", "Ciencia Ficción"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/24151"
+        }
+    }
+};
