52299bd4a94859f68b3cc7f43603be6de6e5a3ea
KAERGSTY <137520623+KAERGSTY@users.noreply.github.com>
2026-03-22 03:41:32 -0500

    O

---
+++ animeO.js
@@ -0,0 +1,336 @@
+window.base["One Piece"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "ワンピース",
+        "Ingles": "One Piece"
+    },
+    sinopsis: "Monkey D. Luffy se niega a permitir que nadie se interponga en su camino para convertirse en el Rey de los Piratas. Con un curso trazado por las traicioneras aguas del Grand Line, este es un capitán que nunca se rendirá hasta que consiga el tesoro más grande de la tierra: el Legendario One Piece.",
+    generos: ["Acción", "Aventura", "Fantasía"],
+    estado: "En emisión",
+    enlaces: {
+        "Serie": {
+            "Japonés": "https://t.me/Kaergsty/2/43330"
+        }
+    }
+};
+
+window.base["Overlord"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "オーバーロード",
+        "Ingles": "Overlord"
+    },
+    sinopsis: "Cuando un popular juego de realidad virtual cierra sus servidores, un veterano jugador decide no cerrar sesión y es transportado a un mundo nuevo donde se convierte en el ser más poderoso: el soberano de la Gran Tumba de Nazarick.",
+    generos: ["Acción", "Fantasía", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/37507"
+        }
+    }
+};
+
+window.base["Overtake!"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "オーバーテイク！",
+        "Ingles": "Overtake!"
+    },
+    sinopsis: "Kouya Madoka, un fotógrafo independiente, se encuentra en un bache profesional hasta que conoce al joven piloto de F4 Haruka Asahina. Inspirado por su determinación, decide apoyarlo para alcanzar la cima del automovilismo.",
+    generos: ["Deportes", "Drama"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/9115"
+        }
+    }
+};
+
+window.base["Oshi no Ko"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "【推しの子】",
+        "Ingles": "Oshi no Ko"
+    },
+    sinopsis: "Un médico y su paciente renacen como los hijos de su idol favorita, Ai Hoshino. A medida que crecen en el mundo del espectáculo, descubren que el brillo de las estrellas oculta oscuros secretos y venganzas.",
+    generos: ["Drama", "Misterio", "Sobrenatural"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/2140"
+        }
+    }
+};
+
+window.base["Osomatsu-san"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "おそ松さん",
+        "Ingles": "Mr. Osomatsu"
+    },
+    sinopsis: "Los sextillizos Matsuno, ahora adultos pero igual de vagos y traviesos que en su infancia, intentan navegar por la vida moderna mientras se meten en situaciones absurdas y cómicas.",
+    generos: ["Comedia", "Recuentos de la vida"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/49387"
+        }
+    }
+};
+
+window.base["Okashi na Tensei"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "おかしな転生",
+        "Ingles": "Sweet Reincarnation"
+    },
+    sinopsis: "Un talentoso pastelero muere antes de cumplir su sueño de crear el dulce perfecto. Reencarna en un mundo de fantasía como el joven Pastry, decidido a convertir su nueva tierra en un reino lleno de postres deliciosos.",
+    generos: ["Fantasía", "Isekai", "Recuentos de la vida"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/57361"
+        }
+    }
+};
+
+window.base["Ousama Ranking"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "王様ランキング",
+        "Ingles": "Ranking of Kings"
+    },
+    sinopsis: "Bojji es un príncipe sordo y débil que ni siquiera puede empuñar una espada de juguete. Acompañado de su amigo sombra Kage, se embarca en una gran aventura para demostrar que tiene lo necesario para ser el mejor rey.",
+    generos: ["Aventura", "Fantasía", "Drama"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/13495"
+        }
+    }
+};
+
+window.base["Ojou to Banken-kun"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "お嬢と番犬くん",
+        "Ingles": "A Girl & Her Guard Dog"
+    },
+    sinopsis: "Isaku es la nieta de un jefe yakuza que solo quiere una vida escolar normal. Sin embargo, su protector sobreprotector, Keiya, se infiltra en su escuela para cuidarla de cualquier pretendiente.",
+    generos: ["Romance", "Drama"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/8548"
+        }
+    }
+};
+
+window.base["Oniichan wa Oshimai!"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "お兄ちゃんはおしまい！",
+        "Ingles": "ONIMAI: I'm Now Your Sister!"
+    },
+    sinopsis: "Mahiro Oyama es un joven amante de los juegos eróticos cuya vida cambia radicalmente cuando su hermana científica lo convierte en una chica como parte de un experimento social.",
+    generos: ["Comedia", "Recuentos de la vida"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/480"
+        }
+    }
+};
+
+window.base["Onmyou Kaiten Re Birth"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "陰陽開展 Re:Birth",
+        "Ingles": "Onmyou Kaiten Re:Birth"
+    },
+    sinopsis: "En un mundo donde lo espiritual y lo moderno chocan, los onmyouji luchan para mantener el equilibrio entre las dimensiones y proteger a la humanidad de fuerzas oscuras.",
+    generos: ["Acción", "Sobrenatural"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/48956"
+        }
+    }
+};
+
+window.base["ONE PUNCH MAN"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "ワンパンマン",
+        "Ingles": "One Punch Man"
+    },
+    sinopsis: "Saitama es un héroe que puede derrotar a cualquier enemigo de un solo puñetazo. Debido a su abrumador poder, se siente aburrido y busca constantemente un rival que sea capaz de darle una pelea digna.",
+    generos: ["Acción", "Comedia", "Parodia"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/53994"
+        }
+    }
+};
+
+window.base["Ore dake level up na ken"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "俺だけレベルアップな件",
+        "Ingles": "Solo Leveling"
+    },
+    sinopsis: "En un mundo donde cazadores humanos deben luchar contra monstruos, Sung Jin-woo es conocido como el cazador más débil. Tras sobrevivir a una mazmorra doble, obtiene un sistema único que solo él puede ver y que le permite subir de nivel sin límites.",
+    generos: ["Acción", "Fantasía", "Aventura"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/12965",
+            "Latino": "https://t.me/Kaergsty/2154/13609"
+        }
+    }
+};
+
+window.base["One Piece Gyojin Tou-hen"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "ワンピース 魚人島編",
+        "Ingles": "One Piece: Fish-Man Island Arc"
+    },
+    sinopsis: "Tras dos años de entrenamiento, la tripulación de Sombrero de Paja se reúne para viajar a la Isla de los Hombres Pez, situada a 10,000 metros bajo el nivel del mar, enfrentando nuevos peligros y prejuicios antiguos.",
+    generos: ["Acción", "Aventura", "Fantasía"],
+    estado: "Finalizado",
+    enlaces: {
+        "Arco": {
+            "Japonés": "https://t.me/Kaergsty/2/29989"
+        }
+    }
+};
+
+window.base["Oroka na Tenshi wa Akuma to Odoru"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "愚かな天使は悪魔と踊る",
+        "Ingles": "The Foolish Angel Dances with the Devil"
+    },
+    sinopsis: "Masatora Akutsu es un demonio que llega a la Tierra buscando aliados para el Infierno. Se fija en la bella Lily Amane, sin saber que ella es en realidad un ángel con sus propios planes para él.",
+    generos: ["Comedia", "Romance", "Sobrenatural"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/13050"
+        }
+    }
+};
+
+window.base["One Room, Hiatari Futsuu, Tenshi-tsuki"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "ワンルーム、日当たり普通、天使つき。",
+        "Ingles": "Studio Apartment, Good Lighting, Angel Included"
+    },
+    sinopsis: "Shintaro es un estudiante que vive solo hasta que descubre a una chica llamada Towa durmiendo en su balcón. Ella resulta ser un ángel que ha venido a la Tierra para aprender sobre la humanidad.",
+    generos: ["Comedia", "Romance", "Sobrenatural"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/17110"
+        }
+    }
+};
+
+window.base["Otonari no Tenshi-sama"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "お隣の天使様にいつの間にか駄目人間をされていた件",
+        "Ingles": "The Angel Next Door Spoils Me Rotten"
+    },
+    sinopsis: "Amane vive solo en un apartamento descuidado. Su vecina, Mahiru, la chica más hermosa de la escuela, decide cuidarlo y cocinarle tras verlo bajo la lluvia, iniciando una dulce relación entre ambos.",
+    generos: ["Romance", "Recuentos de la vida"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/224"
+        }
+    }
+};
+
+window.base["Ore wa Seikan Kokka no Akutoku Ryoushu!"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "俺は星間国家の悪徳領主！",
+        "Ingles": "I'm the Evil Lord of an Intergalactic Empire!"
+    },
+    sinopsis: "Liam reencarna en un imperio galáctico. Habiendo sido traicionado en su vida anterior, decide convertirse en un señor malvado para que nadie lo vuelva a pisotear, aunque sus acciones terminan ayudando a todos por error.",
+    generos: ["Acción", "Ciencia Ficción", "Isekai"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/43341"
+        }
+    }
+};
+
+window.base["Otome Game Sekai wa Mob ni Kibishii"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "乙女ゲー世界はモブに厳しい世界です",
+        "Ingles": "Trapped in a Dating Sim: The World of Otome Games is Tough for Mobs"
+    },
+    sinopsis: "Leon reencarna en un juego otome donde las mujeres mandan y los hombres son simples herramientas. Usando su conocimiento del juego, decide rebelarse contra la aristocracia para vivir una vida tranquila.",
+    generos: ["Fantasía", "Isekai", "Mecha"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/17532"
+        }
+    }
+};
+
+window.base["Ookami to Koushinryou"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "狼と香辛料 MERCHANT MEETS THE WISE WOLF",
+        "Ingles": "Spice and Wolf: Merchant Meets the Wise Wolf"
+    },
+    sinopsis: "El mercader Lawrence encuentra a Holo, una deidad lobo de la cosecha, en su carreta. Juntos emprenden un viaje a través de tierras medievales, combinando el comercio y la economía con una creciente relación personal.",
+    generos: ["Aventura", "Fantasía", "Romance"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/17245"
+        }
+    }
+};
+
+window.base["Okinawa de Suki ni Natta Ko"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "沖縄で好きになった子が方言すぎてつらすぎる",
+        "Ingles": "The Girl I Fell in Love with in Okinawa Has Too Much Dialect"
+    },
+    sinopsis: "Teruaki se muda a Okinawa y se enamora de Hina, pero tiene un gran problema: no entiende nada de lo que ella dice debido a su fuerte dialecto local. Por suerte, su amiga Kana siempre está ahí para traducir.",
+    generos: ["Comedia", "Romance"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/34766"
+        }
+    }
+};
+
+window.base["Ore wa Subete wo Parry suru"] = {
+    carpeta: "imgO",
+    nombresAlternativos: {
+        "Japonés": "俺は全てを【パリイ】する",
+        "Ingles": "I Parry Everything"
+    },
+    sinopsis: "Noor cree que no tiene talento porque solo ha aprendido a desviar ataques. Sin saber que su habilidad de 'parry' ha alcanzado niveles divinos, comienza a realizar hazañas heroicas pensando que es lo más normal del mundo.",
+    generos: ["Acción", "Aventura", "Fantasía"],
+    estado: "Finalizado",
+    enlaces: {
+        "Temporada 1": {
+            "Japonés": "https://t.me/Kaergsty/2/23490"
+        }
+    }
+};
