7819f19e54d3ecf442cd15065782d5e30e1aec5a
KAERGSTY <137520623+KAERGSTY@users.noreply.github.com>
2026-03-23 03:52:30 -0500

    T

---
+++ animeT.js
@@ -0,0 +1,506 @@
+window.base["Temple"] = {
+    carpeta: "imgT",
+    sinopsis: "Akagami Akemitsu decide convertirse en monje para alejarse de los deseos mundanos, pero el templo al que llega está lleno de hermosas chicas.",
+    estado: "Finalizado",
+    generos: ["Comedia", "Romance", "Ecchi"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/7899"
+        }
+    }
+};
+
+window.base["Turkey!"] = {
+    carpeta: "imgT",
+    sinopsis: "Una historia original sobre un grupo de chicas de secundaria que descubren su pasión y amistad a través del juego de bolos.",
+    estado: "Próximamente",
+    generos: ["Deportes", "Recuentos de la vida"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/49388"
+        }
+    }
+};
+
+window.base["Teogonia"] = {
+    carpeta: "imgT",
+    sinopsis: "En un mundo duro y cruel, un joven lucha por sobrevivir contra monstruos brutales mientras descubre los misterios de su propia existencia y los dioses.",
+    estado: "Próximamente",
+    generos: ["Acción", "Fantasía", "Aventura"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/43346"
+        }
+    }
+};
+
+window.base["Tasuuketsu"] = {
+    carpeta: "imgT",
+    sinopsis: "Un misterioso juego de supervivencia comienza de repente. Los participantes deben votar, y la mayoría... muere. Solo la minoría sobrevive.",
+    estado: "Finalizado",
+    generos: ["Suspense", "Psicológico", "Misterio"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/23395"
+        }
+    }
+};
+
+window.base["To Be Hero X"] = {
+    carpeta: "imgT",
+    sinopsis: "En un mundo donde la fe de la gente crea héroes, un torneo se lleva a cabo para determinar quién es digno de llevar el título de 'Hero X'.",
+    estado: "Próximamente",
+    generos: ["Acción", "Sci-Fi", "Superpoderes"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/43352"
+        }
+    }
+};
+
+window.base["True Beauty"] = {
+    carpeta: "imgT",
+    sinopsis: "Una estudiante de secundaria que sufre acoso por su apariencia descubre el poder del maquillaje y se transforma en la chica más popular de la escuela.",
+    estado: "Finalizado",
+    generos: ["Romance", "Drama", "Escolar"],
+    enlaces: {
+        "Temporada 1": {
+            "Latino 🇲🇽": "https://t.me/Kaergsty/2/32190"
+        }
+    }
+};
+
+window.base["Tougen Anki"] = {
+    carpeta: "imgT",
+    sinopsis: "Shiki Ichinose descubre que tiene sangre de Oni (demonio). Tras una tragedia, se inscribe en la Academia Rakshasa para aprender a controlar sus poderes y luchar contra los Momotaro.",
+    estado: "Próximamente",
+    generos: ["Acción", "Sobrenatural", "Fantasía"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/49406"
+        }
+    }
+};
+
+window.base["Trillion Game"] = {
+    carpeta: "imgT",
+    sinopsis: "Dos amigos muy diferentes, uno calculador y otro impulsivo, se proponen ganar un billón de dólares partiendo desde cero en el competitivo mundo empresarial.",
+    estado: "En emisión",
+    generos: ["Drama", "Suspenso"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/28766"
+        }
+    }
+};
+
+window.base["The New Gate"] = {
+    carpeta: "imgT",
+    sinopsis: "Tras derrotar al jefe final de un juego de realidad virtual mortal, Shin es transportado 500 años al futuro dentro del mismo mundo del juego, que ahora es real.",
+    estado: "Finalizado",
+    generos: ["Acción", "Aventura", "Isekai"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/17738"
+        }
+    }
+};
+
+window.base["Tasokare Hotel"] = {
+    carpeta: "imgT",
+    sinopsis: "Neko Tsukahara despierta en un misterioso hotel sin saber si está viva o muerta. Allí ayuda a los huéspedes a recordar su pasado y descubrir su verdadero destino.",
+    estado: "Próximamente",
+    generos: ["Misterio", "Sobrenatural"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/35985"
+        }
+    }
+};
+
+window.base["Tomodachi Game"] = {
+    carpeta: "imgT",
+    sinopsis: "Un grupo de amigos es secuestrado y obligado a participar en una serie de juegos retorcidos que pondrán a prueba su confianza y revelarán oscuros secretos para saldar una deuda.",
+    estado: "Finalizado",
+    generos: ["Psicológico", "Suspense", "Drama"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/49643"
+        }
+    }
+};
+
+window.base["Tadaima, Okaeri"] = {
+    carpeta: "imgT",
+    sinopsis: "Masaki, un omega, y su esposo alfa, Hiromu, crían juntos a su adorable hijo Hikari mientras superan los prejuicios de la sociedad y disfrutan de su amorosa vida familiar.",
+    estado: "Finalizado",
+    generos: ["Boys Love", "Recuentos de la vida", "Romance"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/17597"
+        }
+    }
+};
+
+window.base["Takopi's Original Sin"] = {
+    carpeta: "imgT",
+    sinopsis: "Un inocente alienígena parecido a un pulpo aterriza en la Tierra y se hace amigo de una niña llamada Shizuka. Intenta hacerla feliz, pero no comprende la oscura y cruel realidad de su vida.",
+    estado: "Próximamente",
+    generos: ["Drama", "Psicológico", "Sci-Fi"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/48835"
+        }
+    }
+};
+
+window.base["Tasogare Out Focus"] = {
+    carpeta: "imgT",
+    sinopsis: "Mao y Hisashi son compañeros de cuarto en la residencia escolar con reglas estrictas. Una de ellas es no revelar el secreto de Hisashi, lo que lleva a un complejo romance entre ellos.",
+    estado: "Finalizado",
+    generos: ["Boys Love", "Escolar", "Drama"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/23489"
+        }
+    }
+};
+
+window.base["Tonari no Youkai-san"] = {
+    carpeta: "imgT",
+    sinopsis: "En un pueblo donde humanos y yokais conviven en armonía, seguimos la tranquila pero misteriosa vida diaria de sus habitantes y las conexiones que forman.",
+    estado: "Finalizado",
+    generos: ["Recuentos de la vida", "Sobrenatural"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/17472"
+        }
+    }
+};
+
+window.base["Tsuyokute New Saga"] = {
+    carpeta: "imgT",
+    sinopsis: "Tras derrotar al Rey Demonio pero perder a todos sus seres queridos, el héroe Kail retrocede en el tiempo. Ahora usará sus conocimientos futuros para salvar a todos en su 'nueva partida'.",
+    estado: "Próximamente",
+    generos: ["Fantasía", "Acción", "Aventura"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/48957"
+        }
+    }
+};
+
+window.base["Tengoku Daimakyou"] = {
+    carpeta: "imgT",
+    sinopsis: "En un Japón postapocalíptico plagado de monstruos, Maru y Kiruko viajan en busca de un lugar llamado 'El Edén', mientras un grupo de niños vive aislado en unas extrañas instalaciones.",
+    estado: "Finalizado",
+    generos: ["Sci-Fi", "Aventura", "Misterio"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/661",
+            "Latino 🇲🇽": "https://t.me/Kaergsty/2154/4770"
+        }
+    }
+};
+
+window.base["Tensui no Sakuna-hime"] = {
+    carpeta: "imgT",
+    sinopsis: "La diosa de la cosecha Sakuna es desterrada a una isla peligrosa. Allí, deberá aprender a cultivar arroz y enfrentarse a demonios para proteger a un grupo de humanos marginados.",
+    estado: "Finalizado",
+    generos: ["Fantasía", "Acción"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/23645"
+        }
+    }
+};
+
+window.base["Tensei shitara Ken Deshita"] = {
+    carpeta: "imgT",
+    sinopsis: "Un humano reencarna en un mundo de fantasía, no como un héroe, sino como una espada inteligente. Pronto se asocia con Fran, una joven chica bestia esclava a la que ayuda a liberarse.",
+    estado: "Finalizado",
+    generos: ["Isekai", "Acción", "Fantasía"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/662"
+        }
+    }
+};
+
+window.base["Tsue to Tsurugi no Wistoria"] = {
+    carpeta: "imgT",
+    sinopsis: "Will asiste a una academia de magia, pero carece de la habilidad para lanzar hechizos. Sin embargo, lo compensa con una destreza abrumadora con la espada para cumplir una promesa a su amiga de la infancia.",
+    estado: "Finalizado",
+    generos: ["Acción", "Fantasía", "Magia"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/23826"
+        }
+    }
+};
+
+window.base["Tonikaku Kawaii"] = {
+    carpeta: "imgT",
+    sinopsis: "La vida de recién casados de Nasa y Tsukasa Yuzaki continúa llena de momentos dulces, cómicos y llenos de amor mientras profundizan su relación.",
+    estado: "Finalizado",
+    generos: ["Romance", "Comedia", "Recuentos de la vida"],
+    enlaces: {
+        "Temporada 2": {
+            "Sub Español": "https://t.me/Kaergsty/2/719"
+        }
+    }
+};
+
+window.base["Tsuma, Shougakusei ni Naru"] = {
+    carpeta: "imgT",
+    sinopsis: "Diez años después de la muerte de su esposa, un hombre deprimido y su hija reciben la visita de una estudiante de primaria que afirma ser la reencarnación de su difunta esposa.",
+    estado: "En emisión",
+    generos: ["Drama", "Sobrenatural", "Recuentos de la vida"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/28908"
+        }
+    }
+};
+
+window.base["Tomo-chan wa Onnanoko"] = {
+    carpeta: "imgT",
+    sinopsis: "Tomo Aizawa es una chica con un comportamiento muy masculino que está perdidamente enamorada de su amigo de la infancia, Junichiro. Sin embargo, él la ve solo como a uno más de los chicos.",
+    estado: "Finalizado",
+    generos: ["Comedia", "Romance", "Escolar"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/722",
+            "Latino 🇲🇽": "https://t.me/Kaergsty/2154/12767"
+        }
+    }
+};
+
+window.base["Tearmoon Teikoku Monogatari"] = {
+    carpeta: "imgT",
+    sinopsis: "Mia, la princesa egoísta del Imperio Tearmoon, es ejecutada en la guillotina... pero despierta años en el pasado. Ahora intentará arreglar el imperio, no por nobleza, ¡sino para salvar su propio cuello!",
+    estado: "Finalizado",
+    generos: ["Fantasía", "Comedia", "Reencarnación"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/8760"
+        }
+    }
+};
+
+window.base["Taiyou yori mo Mabushii Hoshi"] = {
+    carpeta: "imgT",
+    sinopsis: "Sakuya es una chica fuerte y alta que siempre ha estado enamorada de su frágil amigo de la infancia. Al llegar a la secundaria, sus roles parecen cambiar, pero sus sentimientos se mantienen.",
+    estado: "Próximamente",
+    generos: ["Romance", "Escolar", "Shojo"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/53905"
+        }
+    }
+};
+
+window.base["Tokyo Revengers Tenjiku-hen"] = {
+    carpeta: "imgT",
+    sinopsis: "Takemichi Hanagaki continúa sus viajes en el tiempo. Esta vez, la pandilla Tokyo Manji se enfrenta a su mayor amenaza hasta el momento: la brutal pandilla Tenjiku liderada por Izana Kurokawa.",
+    estado: "Finalizado",
+    generos: ["Acción", "Drama", "Viajes en el tiempo"],
+    enlaces: {
+        "Temporada 3": {
+            "Sub Español": "https://t.me/Kaergsty/2/8599"
+        }
+    }
+};
+
+window.base["Tate no Yuusha"] = {
+    carpeta: "imgT",
+    sinopsis: "Naofumi es invocado a otro mundo como el Héroe del Escudo. Tras ser traicionado, debe comenzar desde abajo, reclutando a Raphtalia y Filo para salvar el mundo que le dio la espalda.",
+    estado: "Finalizado",
+    generos: ["Isekai", "Acción", "Fantasía"],
+    enlaces: {
+        "Temporada 3": {
+            "Sub Español": "https://t.me/Kaergsty/2/8696"
+        }
+    }
+};
+
+window.base["Taishou Otome Otogibanashi"] = {
+    carpeta: "imgT",
+    sinopsis: "Japón, era Taisho. Tamahiko es desterrado de su familia adinerada tras perder un brazo. Su vida solitaria cambia cuando Yuzuki, una joven comprada para ser su esposa, llega para cuidar de él.",
+    estado: "Finalizado",
+    generos: ["Romance", "Histórico", "Drama"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/41863"
+        }
+    }
+};
+
+window.base["Tsuihousha Shokudou e Youkoso!"] = {
+    carpeta: "imgT",
+    sinopsis: "Un talentoso cocinero es expulsado de su grupo de aventureros. Decide abrir su propio restaurante en la frontera, donde su increíble comida atrae a toda clase de exiliados y seres curiosos.",
+    estado: "Próximamente",
+    generos: ["Fantasía", "Gourmet", "Recuentos de la vida"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/48984"
+        }
+    }
+};
+
+window.base["Tsuki ga Michibiku Isekai Douchuu"] = {
+    carpeta: "imgT",
+    sinopsis: "Makoto Misumi es invocado a otro mundo, pero la diosa lo rechaza por ser 'feo' y lo arroja a los confines de la tierra. Allí, descubrirá que tiene habilidades mágicas fuera de lo normal.",
+    estado: "Finalizado",
+    generos: ["Isekai", "Fantasía", "Comedia"],
+    enlaces: {
+        "Temporada 2": {
+            "Sub Español": "https://t.me/Kaergsty/2/19357",
+            "Latino 🇲🇽": "https://t.me/Kaergsty/2154/19358"
+        }
+    }
+};
+
+window.base["Toaru Ossan no VRMMO Katsudouki"] = {
+    carpeta: "imgT",
+    sinopsis: "Taichi Tanaka, un hombre de 38 años, juega a un nuevo VRMMO. En lugar de buscar la gloria, se dedica a usar habilidades consideradas inútiles, cocinando y fabricando objetos a su propio ritmo.",
+    estado: "Finalizado",
+    generos: ["Acción", "Aventura", "Videojuegos"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/8559"
+        }
+    }
+};
+
+window.base["Tensei Kizoku no Isekai Boukenroku"] = {
+    carpeta: "imgT",
+    sinopsis: "Kazuya Shiina reencarna como Cain von Silford en un mundo de magia. Los dioses le otorgan protecciones divinas excesivas, convirtiéndolo en un noble con un poder desproporcionado.",
+    estado: "Finalizado",
+    generos: ["Isekai", "Fantasía", "Comedia"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/502"
+        }
+    }
+};
+
+window.base["Tensei shitara Dainana Ouji Datta node"] = {
+    carpeta: "imgT",
+    sinopsis: "Un mago ordinario reencarna como Lloyd, el séptimo príncipe de un reino poderoso. Conservando sus recuerdos, finalmente tiene la posición y los recursos para estudiar la magia a su antojo.",
+    estado: "Finalizado",
+    generos: ["Fantasía", "Magia", "Aventura"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/17244"
+        }
+    }
+};
+
+window.base["Tensei Kizoku, Kantei Skill de Nariagaru"] = {
+    carpeta: "imgT",
+    sinopsis: "Ars Louvent reencarna como un noble débil en otro mundo, pero posee la habilidad única de 'Evaluar' el potencial de las personas. Usará este poder para reclutar talentos y fortalecer su territorio.",
+    estado: "Finalizado",
+    generos: ["Isekai", "Fantasía", "Estrategia"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/17513"
+        }
+    }
+};
+
+window.base["Tondemo Skill de Isekai Hourou Meshi"] = {
+    carpeta: "imgT",
+    sinopsis: "Mukouda es invocado por error a un mundo de fantasía. Su única habilidad es el 'Supermercado Online'. Al cocinar platillos modernos de Japón, atrae y doma a bestias legendarias como un Fenrir.",
+    estado: "Finalizado",
+    generos: ["Isekai", "Gourmet", "Aventura"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/500"
+        }
+    }
+};
+
+window.base["Tensei shitara Slime Datta Ken 3rd Season"] = {
+    carpeta: "imgT",
+    sinopsis: "Rimuru Tempest, convertido en Señor Demonio, debe lidiar con las consecuencias políticas y enfrentar la hostilidad del Sacro Imperio de Lubelius en esta tercera temporada.",
+    estado: "Finalizado",
+    generos: ["Isekai", "Fantasía", "Acción"],
+    enlaces: {
+        "Temporada 3": {
+            "Sub Español": "https://t.me/Kaergsty/2/17429"
+        }
+    }
+};
+
+window.base["Touken Ranbu Kai Kyoden Moyuru Honnouji"] = {
+    carpeta: "imgT",
+    sinopsis: "Un ejército de espadas históricas personificadas como guerreros viajan en el tiempo al Incidente de Honnouji para proteger la historia de fuerzas oscuras que intentan alterarla.",
+    estado: "Finalizado",
+    generos: ["Acción", "Fantasía", "Histórico"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/17982"
+        }
+    }
+};
+
+window.base["Tokidoki Bosotto Russia-go de Dereru Tonari no Alya-san"] = {
+    carpeta: "imgT",
+    sinopsis: "Alya, una hermosa estudiante medio rusa, a veces murmura cosas dulces en ruso pensando que su compañero Masachika no la entiende. ¡Pero él entiende perfectamente el idioma!",
+    estado: "Finalizado",
+    generos: ["Comedia", "Romance", "Escolar"],
+    enlaces: {
+        "Temporada 1": {
+            "Multi-Audio 🇲🇽 🇯🇵": "https://t.me/Kaergsty/2/23408"
+        }
+    }
+};
+
+window.base["Towa no Yuugure"] = {
+    carpeta: "imgT",
+    sinopsis: "Una historia misteriosa y emotiva ambientada en un crepúsculo interminable, donde los personajes buscan respuestas sobre su existencia.",
+    estado: "Próximamente",
+    generos: ["Drama", "Fantasía", "Misterio"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/53908"
+        }
+    }
+};
+
+window.base["Tojima Tanzaburo wa Kamen Rider ni Naritai"] = {
+    carpeta: "imgT",
+    sinopsis: "Tanzaburo es un fan acérrimo de Kamen Rider. Aunque ya es un adulto, decide usar su entrenamiento y pasión para actuar como un héroe de la vida real combatiendo el crimen a su manera.",
+    estado: "Próximamente",
+    generos: ["Comedia", "Acción"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/53967"
+        }
+    }
+};
+
+window.base["Tensei Akujo no Kuro Rekishi"] = {
+    carpeta: "imgT",
+    sinopsis: "Konoha reencarna en la historia que ella misma escribió en la secundaria, llena de clichés oscuros. Ahora debe sobrevivir a su propio 'historial oscuro' como la villana.",
+    estado: "Próximamente",
+    generos: ["Isekai", "Comedia", "Romance"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/54358"
+        }
+    }
+};
+
+window.base["Tomodachi no Imouto ga Ore ni dake Uzai"] = {
+    carpeta: "imgT",
+    sinopsis: "Akihiro disfruta de una vida tranquila hasta que Iroha, la hermana menor de su mejor amigo, empieza a molestarlo constantemente, aunque solo actúa así frente a él.",
+    estado: "Próximamente",
+    generos: ["Comedia", "Romance"],
+    enlaces: {
+        "Temporada 1": {
+            "Sub Español": "https://t.me/Kaergsty/2/53966"
+        }
+    }
+};
