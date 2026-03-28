d5298662a1dfd16cd1ae448b8209731ea7b3f148
KAERGSTY <137520623+KAERGSTY@users.noreply.github.com>
2026-03-28 07:37:57 -0500

    s

---
+++ animeTODO.js
@@ -0,0 +1,24 @@
+const animesTodo = [
+  {
+    "id": 1,
+    "created_at": "2026-03-28T11:11:11.509103+00:00",
+    "nombre": "Dragon Ball",
+    "sinopsis": "aaaaaaaaaaaaaaaaaa",
+    "portada": "https://m.media-amazon.com/images/S/pv-target-images/334f00b53cf3ef848ea7048b25711bc98e8236ce1685a096990c80d0965835ea.png",
+    "estado": "Finzalizado",
+    "carpeta": "ImgD",
+    "generos": "[\"Accion\",\"peleas\"]",
+    "temporadas_data": []
+  },
+  {
+    "id": 2,
+    "created_at": "2026-03-28T12:19:39.243317+00:00",
+    "nombre": "Dragon Ball",
+    "sinopsis": "aaaaaaaaaaaaaaaaaa",
+    "portada": "https://m.media-amazon.com/images/S/pv-target-images/334f00b53cf3ef848ea7048b25711bc98e8236ce1685a096990c80d0965835ea.png",
+    "estado": "Finzalizado",
+    "carpeta": "ImgD",
+    "generos": "[\"Accion\",\"peleas\"]",
+    "temporadas_data": []
+  }
+];
